5536627ef5cfa586e229e4c8e484c46b34148ba1
Alex <112724167+klimbass@users.noreply.github.com>
2024-02-28 22:57:18 +0200

    format_prettier

--- src/js/showTime.js
+++ src/js/showTime.js
@@ -1,36 +1,27 @@
-const timeCard = "<div class='time-card'><div class='time'><span class='hours'></span><span id='dots'>_</span><span class='min'></span></div><div class='date'>_</div></div>"
-const dataShow = document.querySelector('.date-show')
-dataShow.insertAdjacentHTML('afterbegin', timeCard)
-const timeHTML = document.querySelector('.time')
-const dateHTML = document.querySelector('.date')
+const timeCard =
+  "<div class='time-card'><div class='time'><span class='hours'></span><span id='dots'>_</span><span class='min'></span></div><div class='date'>_</div></div>";
+const dataShow = document.querySelector('.date-show');
+const timeHTML = document.querySelector('.time');
+const dateHTML = document.querySelector('.date');
 const dotsElement = document.getElementById('dots');
-const min = document.querySelector('.min')
-const hours = document.querySelector('.hours')
+const min = document.querySelector('.min');
+const hours = document.querySelector('.hours');
 
-
+dataShow.insertAdjacentHTML('afterbegin', timeCard);
 
 function showTime() {
-   
+  setInterval(() => {
+    const time = new Date();
+    const h = time.getHours();
+    const m = time.getMinutes();
 
-    setInterval(() => {
-        
-       
-        const time = new Date()
-        const h = time.getHours()
-        const m = time.getMinutes()
-
-        
-
-
-        hours.textContent = h
-        min.textContent = m
-        const date = new Date().toLocaleString(navigator.language, { dateStyle: 'short' })
-        dateHTML.textContent = date
-        dotsElement.textContent = dotsElement.textContent === ':' ? ' ' : ':';
-
-        // console.log(time, date);
-        
-    }, 1000)
-
+    hours.textContent = h;
+    min.textContent = m;
+    const date = new Date().toLocaleString(navigator.language, {
+      dateStyle: 'short',
+    });
+    dateHTML.textContent = date;
+    dotsElement.textContent = dotsElement.textContent === ':' ? ' ' : ':';
+  }, 1000);
 }
- showTime()
+showTime();
--- src/main.js
+++ src/main.js
@@ -1,73 +1,64 @@
-import './js/showTime'
+import './js/showTime';
 
+const form = document.querySelector('.form');
+const img = document.querySelector('.image');
+const title = document.querySelector('.title');
+const curWeather = document.querySelector('.current-weather');
+const date = document.querySelector('.date-show span');
+const btnSubmit = document.querySelector('#btn-submit');
+btnSubmit.disabled = true;
 
-
-const form = document.querySelector('.form')
-const img = document.querySelector('.image')
-const title = document.querySelector('.title')
-const curWeather = document.querySelector('.current-weather')
-const date = document.querySelector('.date-show span')
-const btnSubmit = document.querySelector('#btn-submit')
-btnSubmit.disabled = true
-
-const url = 'https://api.openweathermap.org/data/2.5/weather?'
-let icon
+const url = 'https://api.openweathermap.org/data/2.5/weather?';
+let icon;
 const option = new URLSearchParams({
-    appid: '117bcd5592950f9f701a11f5ab982de0',
-    units: 'metric',
-    q: ''
-})
-// date.textContent = new Date().toLocaleString()
+  appid: '117bcd5592950f9f701a11f5ab982de0',
+  units: 'metric',
+  q: '',
+});
 
+form.addEventListener('input', event => {
+  if (event.target.value.trim()) {
+    btnSubmit.disabled = false;
+  }
+});
 
-form.addEventListener('input', (event) => {
-    // console.dir(event.target.value.trim())
-    if (event.target.value.trim()) {
-       btnSubmit.disabled = false    
-    }
-})
-
-
-form.addEventListener('submit', handleSubmit)
-
-
+form.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
-    event.preventDefault()
-    img.textContent = ''
-    option.set('q', event.target.city.value.trim())
-    console.log(option.get('q'));
-    if (option.get('q')) {
-        fetch(url + option)
-        .then(res => {
-            if (!res.ok) {
-                return new Error(`Error: ${res}`)
-            }
-            return res.json()
-        })
-        .then(data => {
-            // console.log(data);
-            title.textContent = data.name + ' ' + data.sys.country;
-            // console.log(data.sys.country);
-            // console.log(data.weather[0].main);
-            icon = data.weather[0].icon;
-            // console.log(icon);
-            const iconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`
-            img.insertAdjacentHTML('afterbegin', `<img src="${iconUrl}" width="80" height="80" />`)
-            console.log(data.main.temp_min, data.main.temp_max);
-            let tempMin = Math.round(data.main.temp_min)
-            if (tempMin > 0) {
-                tempMin = `+${ tempMin }`
-            }
-            let tempMax = Math.round(data.main.temp_max)
-            if (tempMax > 0) {
-                tempMax = `+${ tempMax }`
-            }
-            curWeather.textContent = `${tempMin} - ${tempMax}`
-
-        })
-            .catch(()=>{console.log('Sorry, such city not found!');})
-        .finally(()=> form.reset())
-    }
-    
+  event.preventDefault();
+  img.textContent = '';
+  option.set('q', event.target.city.value.trim());
+  console.log(option.get('q'));
+  if (option.get('q')) {
+    fetch(url + option)
+      .then(res => {
+        if (!res.ok) {
+          return new Error(`Error: ${res}`);
+        }
+        return res.json();
+      })
+      .then(data => {
+        title.textContent = data.name + ' ' + data.sys.country;
+        icon = data.weather[0].icon;
+        const iconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+        img.insertAdjacentHTML(
+          'afterbegin',
+          `<img src="${iconUrl}" width="80" height="80" />`
+        );
+        console.log(data.main.temp_min, data.main.temp_max);
+        let tempMin = Math.round(data.main.temp_min);
+        if (tempMin > 0) {
+          tempMin = `+${tempMin}`;
+        }
+        let tempMax = Math.round(data.main.temp_max);
+        if (tempMax > 0) {
+          tempMax = `+${tempMax}`;
+        }
+        curWeather.textContent = `${tempMin} - ${tempMax}`;
+      })
+      .catch(() => {
+        console.log('Sorry, such city not found!');
+      })
+      .finally(() => form.reset());
+  }
 }
